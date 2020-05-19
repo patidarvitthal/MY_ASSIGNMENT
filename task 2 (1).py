b6f3a8581afbd9f583637e440a8c53d3978d13d7
patidarvitthal <42809313+patidarvitthal@users.noreply.github.com>
2020-05-19 17:29:12 +0530

    Add files via upload

---
+++ task 2 (1).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+for i in range(2000,3201):
+    if(i%7==0 and i%5!=0):
+        print(i,end=',')
+    
+
+
+# In[9]:
+
+
+first_name=input('please enter your first name')
+last_name=input('please enter your last name')
+print(last_name,' ',first_name)
+
+
+# In[11]:
+
+
+import math
+diameter=12
+print((4/3)*(math.pi)*(diameter/2)**3)
+
+
+# In[ ]:
+
+
+
+
